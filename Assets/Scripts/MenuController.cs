bd111b52dadf5220065c229425d6f6ab19d5e46d
loneNerd <alexander.the.great.21356@gmail.com>
2018-04-06 19:04:23 +0300

    Updated scale mode

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MenuController : MonoBehaviour {
+public class MenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("PlayTable");
+    }
 
 	public void Exit()
 	{
